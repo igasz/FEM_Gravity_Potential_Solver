e09350ad795103f43484a5c98739014c8a4f9e49
igasz <igaszaf@gmail.com>
2026-01-28 18:58:36 +0100

    poprawki

--- zad_obliczeniowe.py
+++ zad_obliczeniowe.py
@@ -16,8 +16,16 @@ def solve():
 
     D = 3 #dziedzina
     G = 1
-    h = D/n
-    nodes = np.linspace(0, D, n+1) #pkt pomiędzy przedziałami
+    h = D/n # b - a
+    nodes = np.linspace(0, D, n+1) # pkt pomiędzy przedziałami
+
+    # warunki dirichleta
+    PHI_LEFT = 5
+    PHI_RIGHT = 2
+
+    # cx + d
+    c = (PHI_RIGHT - PHI_LEFT) / D
+    d = PHI_LEFT
 
     # funkcja gęstości
     def rho(x):
@@ -34,56 +42,55 @@ def solve():
 
     # całki liczone numerycznie
     def d_phi_thilde(x):
-        return -1
+        return c
 
     def integral_L(node_type, el_idx):
         res = 0.0
-        x_start = nodes[el_idx]
+        a = nodes[el_idx]
 
-        for p, w in zip(gauss_pts, gauss_w):
-            x_mapped = x_start + (p + 1) * h / 2
-            val_v = (1 - p) / 2 if node_type == "left" else (1 + p) / 2
-            res += (4 * np.pi * G * rho(x_mapped) * val_v) * (h / 2) * w
+        for p, weight in zip(gauss_pts, gauss_w):
+            x_mapped = a + (p + 1) * h / 2
+            v = (1 - p) / 2 if node_type == "left" else (1 + p) / 2
+            res += (4 * np.pi * G * rho(x_mapped) * v) * (h / 2) * weight
         return res
 
-    def integral_B(node_type, el_idx):
+    def integral_L1(node_type, el_idx):
         res = 0.0
-        x_start = nodes[el_idx]
+        a = nodes[el_idx]
         dv = -1 / h if node_type == "left" else 1 / h
 
-        for p, w in zip(gauss_pts, gauss_w):
-            x_mapped = x_start + (p + 1) * h / 2
-            res += (d_phi_thilde(x_mapped) * dv) * (h / 2) * w
+        for p, weight in zip(gauss_pts, gauss_w):
+            x_mapped = a + (p + 1) * h / 2
+            res += (d_phi_thilde(x_mapped) * dv) * (h / 2) * weight
         return res
 
-    # A*w = b
-    size = n - 1
-    A = np.zeros((size, size))
-    b = np.zeros(size)
+    # budowanie macierzy A*w = b
+    A = np.zeros((n-1, n-1)) # macierz
+    b = np.zeros(n-1) # wektor b
 
     for i in range(1, n):
         idx = i - 1
 
-        A[idx, idx] = -2.0 / h
-        if idx > 0: A[idx, idx - 1] = 1.0 / h
-        if idx < size - 1: A[idx, idx + 1] = 1.0 / h
+        A[idx, idx] = -2 / h
+        if idx > 0: A[idx, idx - 1] = 1 / h
+        if idx < (n-1) - 1: A[idx, idx + 1] = 1 / h
 
         b[idx] += integral_L("right", i - 1)
         b[idx] += integral_L("left", i)
-        b[idx] += integral_B("right", i - 1)
-        b[idx] += integral_B("left", i)
+        b[idx] += integral_L1("right", i - 1)
+        b[idx] += integral_L1("left", i)
 
-
-    w_sol = np.linalg.solve(A, b)
-    phi_sol = np.zeros(n + 1)
+    # rozwiązanie układu równań
+    w = np.linalg.solve(A, b)
+    phi = np.zeros(n + 1)
 
     for i in range(n + 1):
         x = nodes[i]
-        val_shift = -x + 5
+        shift = c * x + d
         if i == 0 or i == n:
-            phi_sol[i] = val_shift
+            phi[i] = shift
         else:
-            phi_sol[i] = w_sol[i - 1] + val_shift
+            phi[i] = w[i - 1] + shift
 
     # wypisywanie wyników obliczeń
     print(f"Liczba elementów (n): {n}")
@@ -91,11 +98,11 @@ def solve():
     print(f"{'Węzeł i':<10} | {'x_i':<10} | {'Phi(x_i)':<10}")
     print("-" * 33)
     for i in range(n + 1):
-        print(f"{i:<10} | {nodes[i]:<10.4f} | {phi_sol[i]:<10.4f}")
+        print(f"{i:<10} | {nodes[i]:<10.4f} | {phi[i]:<10.4f}")
 
     # rysowanie wykresów
     plt.figure(figsize=(10, 6))
-    plt.plot(nodes, phi_sol, 'r-o', label=f'n={n}')
+    plt.plot(nodes, phi, 'r-o', label=f'n={n}')
     plt.xlabel("Pozycja x")
     plt.ylabel("Wartość Phi(x)")
     plt.grid(True)
